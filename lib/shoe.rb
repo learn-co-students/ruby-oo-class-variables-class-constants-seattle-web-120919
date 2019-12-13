5658da07c27356f80d26be325ceef14fb9218d70
github username <github email address>
2019-12-12 23:05:16 -0800

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+    BRANDS 
   end
 
   def cobble
@@ -12,3 +17,4 @@ class Shoe
   end
 
 end
+
